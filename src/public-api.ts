b0aef8e075f7e36d4f4d2e453c98341ddfb5f70a
adam antinoo <adamantinoo.git@gmail.com>
2025-04-15 20:31:53 +0200

    -[CHORE] Update exported classes.

--- src/public-api.ts
+++ src/public-api.ts
@@ -4,12 +4,15 @@
 // - C O N S T A N T S
 export * from "./common/Objects"
 export * from "./common/ObjectValidation"
+
 /*
  * Public API Surface of dimensinfin-core/interfaces
  */
-export * from './interfaces/feature.interface';
-export * from './interfaces/menu.interface';
-export * from './interfaces/dialog.interface';
-export * from './interfaces/service.interface';
-export * from './interfaces/common.interface';
-export * from './interfaces/types';
+export * from './interfaces/IViewer.interface';
+export * from './interfaces/ISelectable.interface';
+export * from './interfaces/IRefreshable.interface';
+export * from './interfaces/INode.interface';
+export * from './interfaces/INamed.interface';
+export * from './interfaces/IExpandable.interface';
+export * from './interfaces/IDtoCompliant.interface';
+export * from './interfaces/ICollaboration.interface';
